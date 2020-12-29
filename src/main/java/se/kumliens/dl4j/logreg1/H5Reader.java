934d023b77b7787b5941bece1b35909bae217ec9
Svante Kumlien <svante.kumlien@gmail.com>
2020-12-29 13:53:03 +0100

    make it work running from jar

--- src/main/java/se/kumliens/dl4j/logreg1/H5Reader.java
+++ src/main/java/se/kumliens/dl4j/logreg1/H5Reader.java
@@ -1,6 +1,9 @@
 package se.kumliens.dl4j.logreg1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,15 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class H5Reader {
 
-    private final ResourceLoader resourceLoader;
-
     @SneakyThrows
-    public Map<String, Object> readH5(Resource fileResource) {
-        log.info("Loading h5 data from {}", fileResource.getURL());
-        File file = fileResource.getFile();
-        log.info("File {} exists: {}", file.getAbsolutePath(), file.exists());
+    public Map<String, Object> readH5(Resource resource) {
+        log.info("Loading h5 data from {}", resource.getURL());
+ 
         Map<String, Object> datasets = new HashMap<>();
-        try (HdfFile hdfFile = new HdfFile(file)) {
+        try (HdfFile hdfFile = HdfFile.fromInputStream(resource.getInputStream())) {
             Map<String, Node> children = hdfFile.getChildren();
             children.keySet().forEach(k -> {
                 Dataset ds = hdfFile.getDatasetByPath(children.get(k).getPath());
